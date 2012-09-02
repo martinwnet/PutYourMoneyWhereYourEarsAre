fd29187bcec77b6e0ef56609b2df3f3d403da046
Martin Walmsley <hello@martinw.net>
2012-09-02 14:34:15 +0100

    Fixing js to remove smoother, adding subtlepattern background and google web font

--- lastfm.js
+++ lastfm.js
@@ -69,7 +69,7 @@ function displayAlbums() {
 function display(album) {
     var albumId = (album.mbid == "") ? albumId = fakeId++ : album.mbid;
     if ($.inArray(albumId, owned) == -1 && $.inArray(albumId, displayed) == -1) {
-        $("#albums").append("<li id=" + albumId + ">" + album.artist.name + " - " + album.name + "<br /><a href='#' id='removeLink' onclick='remove(\"" + albumId + "\");'>own</a> " + "</li>");
+        $("#albums").append("<li id=" + albumId + ">" + album.artist.name + " - " + album.name + "<br /><a href='#' id='removeLink' onclick='remove(\"" + albumId + "\");return false;'>own</a> " + "</li>");
         displayed.push(albumId);
         if (album.image[3]) {
             var imgUrl = album.image[3]["#text"];
